63598bd55469227f2eef22e9c61e9a5e0bfdc37d
hovanduy <duytink32@gmail.com>
2023-08-22 12:13:15 +0700

    Fix comment review

--- nd0191-c1-project1/src/Book.js
+++ nd0191-c1-project1/src/Book.js
@@ -1,50 +1,45 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Book extends Component {
-  state = {
-    value: this.props.shelf
+export default function Book({book, shelf, onMove}) {
+
+  const handleChange = event => {
+    const val = event.target.value;
+    onMove(book, val);
   };
-  handleChange = event => {
-    const { value } = event.target;
-    this.setState({ value });
-    this.props.onMove(this.props.book, value);
-  };
-  render() {
-    return (
-      <li>
-      <div className="book">
-        <div className="book-top">
-          <div
-            className="book-cover"
-            style={{
-              width: 128,
-              height: 193,
-              backgroundImage: `url(${
-                this.props.book.imageLinks
-                  ? this.props.book.imageLinks.thumbnail
-                  : 'icons/book-placeholder.svg'
-              })`
-            }}
-          />
-          <div className="book-shelf-changer">
-            <select value={this.state.value} onChange={this.handleChange}>
-              <option value="move" disabled>
-                Move to...
-              </option>
-              <option value="currentlyReading">Currently Reading</option>
-              <option value="wantToRead">Want to Read</option>
-              <option value="read">Read</option>
-              <option value="none">None</option>
-            </select>
-          </div>
-        </div>
-        <div className="book-title">{this.props.book.title}</div>
-        <div className="book-authors">
-          {this.props.book.authors ? this.props.book.authors.join(', ') : 'Unknown'}
+
+  return (
+    <li>
+    <div className="book">
+      <div className="book-top">
+        <div
+          className="book-cover"
+          style={{
+            width: 128,
+            height: 193,
+            backgroundImage: `url(${
+              book.imageLinks
+                ? book.imageLinks.thumbnail
+                : 'icons/book-placeholder.svg'
+            })`
+          }}
+        />
+        <div className="book-shelf-changer">
+          <select value={shelf} onChange={handleChange}>
+            <option value="move" disabled>
+              Move to...
+            </option>
+            <option value="currentlyReading">Currently Reading</option>
+            <option value="wantToRead">Want to Read</option>
+            <option value="read">Read</option>
+            <option value="none">None</option>
+          </select>
         </div>
       </div>
-    </li>
-    );
-  }
+      <div className="book-title">{book.title}</div>
+      <div className="book-authors">
+        {book.authors ? book.authors.join(', ') : 'Unknown'}
+      </div>
+    </div>
+  </li>
+  ); 
 }
-export default Book;
